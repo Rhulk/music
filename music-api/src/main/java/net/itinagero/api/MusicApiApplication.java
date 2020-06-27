7230ad27ce7a9f601795dc72d03f51e809ae6286
rhulk <quique1904@gmail.com>
2020-06-27 10:37:06 +0200

    Carga de cambios en MusicApiApplication.java

--- music-api/src/main/java/net/itinagero/api/MusicApiApplication.java
+++ music-api/src/main/java/net/itinagero/api/MusicApiApplication.java
@@ -8,8 +8,6 @@ public class MusicApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusicApiApplication.class, args);
-//		prueba git
-		// añadiendo comentario para ver comportamiento en Git
 	}
 
 }
